9e07d658d38aba5b6f933c497ecf791ab3b9af3e
meYashJain <147161720+meYashJain@users.noreply.github.com>
2024-11-27 16:55:21 +0530

    Update urls.py

--- campaign_api/urls.py
+++ campaign_api/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('campaigns.urls')),
 ]
+
+
+# Serve static files in production (when DEBUG=False)
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
